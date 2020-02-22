dc97be8f8ed320002d4ff1095a2fe519c1150990
sxyazi <sxyazi@gmail.com>
2020-02-23 02:23:45 +0800

    add vcodec checker

--- up.py
+++ up.py
@@ -57,11 +57,18 @@ def segment_time(file):
   except Exception:
     return 10
 
+def video_codec(file):
+  codecs = os.popen('ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 %s' % file).read().strip()
+
+  s = set(codecs.split('\n'))
+  return 'copy' if 'h264' in s and len(s) == 1 else 'h264'
+
 def main():
 
   video  = quote(os.path.abspath(sys.argv[1]))
   title  = sys.argv[2] if len(sys.argv)>2 else os.path.splitext(os.path.basename(sys.argv[1]))[0]
   stime  = float(sys.argv[3]) if len(sys.argv)>3 else segment_time(video)
+  vcodec = video_codec(video)
   tmpdir = os.path.dirname(os.path.abspath(__file__)) + '/tmp'
 
   if os.path.isdir(tmpdir):
@@ -71,7 +78,7 @@ def main():
 
   # os.system('ffmpeg -i %s -codec copy -map 0 -f segment -segment_list out.m3u8 -segment_list_flags +live -segment_time 5 out%%03d.ts' % video)
   # os.system('ffmpeg -i %s -vcodec copy -acodec aac -hls_list_size 0 -hls_segment_size 3000000 -f hls out.m3u8' % video)
-  os.system('ffmpeg -i %s -vcodec copy -acodec aac -map 0 -f segment -segment_list out.m3u8 -segment_time %d out%%03d.ts' % (video, stime))
+  os.system('ffmpeg -i %s -vcodec %s -acodec aac -map 0 -f segment -segment_list out.m3u8 -segment_time %d out%%03d.ts' % (video, vcodec, stime))
 
   i, lines = 0, open('out.m3u8', 'r').read()
   executor = ThreadPoolExecutor(max_workers=10)
